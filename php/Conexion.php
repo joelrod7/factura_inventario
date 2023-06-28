de0226e7aec9122ac1e1f5c1ba543be3195cc557
joelrod7 <joeldjesusr@gmail.com>
2023-06-28 11:52:26 -0500

    DB

---
+++ php/Conexion.php
@@ -0,0 +1,9 @@
+<?php
+
+$servidor="localhost";
+$base_datos="facturacion";
+$usuario="root";
+$clave="";
+
+$conexion = new mysqli($servidor, $usuario, $clave);
+mysqli_select_db($conexion, $base_datos);
